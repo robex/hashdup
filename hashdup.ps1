1ec911176a1553863bde847e6c337f58dce97fbc
robex <23484075+robex@users.noreply.github.com>
2018-07-01 13:11:18 -0700

    initial commit

---
+++ hashdup.ps1
@@ -0,0 +1,42 @@
+#############################################
+#                  HASHDUP                  #
+#                                           #
+# Recursively check for duplicate files and #
+#        print colliding full paths.        #
+#                                           #
+#            /robex/ - (C) 2018             #
+#############################################
+
+# USAGE: hashdup [path]
+
+$path = $args[0]
+
+# remove '-Recurse' if dont want deep searching
+$files = Get-ChildItem -Path $path -Recurse -File
+
+$fileHashes = @()
+
+foreach($file in $files) {
+	$thisFileHash = Get-FileHash -Algorithm md5 -Path $file.FullName
+
+	if ($fileHashes.Count -eq 0) {
+		$fileHashes += @($thisFileHash)
+	} else {
+		$isDuplicate = $false
+		foreach ($fileHash in $fileHashes) {
+			if ($fileHash.Hash -eq $thisFileHash.Hash) {
+				$isDuplicate = $true
+				$thisPath = $file.FullName
+				echo "duplicate file: $thisPath"
+				$collidePath = $fileHash.Path
+				echo "duplicate with: $collidePath"
+				echo ""
+			}
+		}
+
+		# if not duplicate, add to the hash list
+		if(!$isDuplicate) {
+			$fileHashes += @($thisFileHash)
+		}
+	}
+}
